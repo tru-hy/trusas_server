f93243ba44f79992985fc21fbdc13c4fd5119ba9
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2013-07-25 01:50:30 +0300

    Numpy array serialization and a helper for easy results

--- serialize.py
+++ serialize.py
@@ -1,8 +1,23 @@
 import simplejson
 
+def _to_nulls(lst):
+	# Hack to convert NaNs and Infs to JSON nulls
+	if not isinstance(lst, list):
+		return lst
+	for i in range(len(lst)):
+		if isinstance(lst[i], list):
+			_to_nulls(lst[i])
+			continue
+		if lst[i] != lst[i]:
+			lst[i] = None
+	return lst
+	
+
 def object_mangler(v):
 	if hasattr(v, 'isoformat'):
 		return v.isoformat()
+	if hasattr(v, 'tolist'):
+		return _to_nulls(v.tolist())
 	raise TypeError("Don't know how to serialize "+repr(v))
 
 def dump(*args, **kwargs):
@@ -12,4 +27,8 @@ def dump(*args, **kwargs):
 def dumps(*args, **kwargs):
 	kwargs['default'] = object_mangler
 	return simplejson.dumps(*args, default=object_mangler)
+
+def result(obj, **kwargs):
+	return ({'Content-Type': 'application/json'}, dumps(obj, **kwargs))
+
 
